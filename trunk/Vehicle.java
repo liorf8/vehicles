69733b874bb429b903a45315306c2cabf928562c
karlr42 <karlr42@001830dc-046a-11de-89a0-d95f4161952e>
2009-04-10 13:28:00 +0000

    Fixed

---
+++ trunk/Vehicle.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ * @author Karl 
+ * A superclass for vehicle, which can be sub-classed for use in the vehicle editors 
+ * or the simulation, while maintaining some common attributes
+ *
+ */
+public class Vehicle {
+	private String xmlLocation; //location of the XML file representing this vehicle
+	private String name; //This vehicle's name
+	
+	public String getXmlLocation() {
+		return xmlLocation;
+	}
+	public void setXmlLocation(String xmlLocation) {
+		this.xmlLocation = xmlLocation;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
