26a4d033db8973beb7343a74842b19e661461d8a
karlr42 <karlr42@001830dc-046a-11de-89a0-d95f4161952e>
2009-04-14 11:28:57 +0000

    Test, sorry

--- vehicles/vehicle/Vehicle.java
+++ vehicles/vehicle/Vehicle.java
@@ -11,8 +11,12 @@ public class Vehicle {
 	private String xmlLocation; //location of the XML file representing this vehicle
 	private String name; //This vehicle's name
 	
+	/**
+	 * 
+	 * @return Location of this object's xml file
+	 */
 	public String getXmlLocation() {
-		return xmlLocation;
+		return xmlLocation; 
 	}
 	public void setXmlLocation(String xmlLocation) {
 		this.xmlLocation = xmlLocation;
