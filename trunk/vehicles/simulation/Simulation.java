b9dae22f841839dcdfe0f5971f5c4612cdba005d
graysr89 <graysr89@001830dc-046a-11de-89a0-d95f4161952e>
2009-04-18 19:32:15 +0000

    Added an editor simulation class, for editing simulations

--- trunk/vehicles/simulation/Simulation.java
+++ trunk/vehicles/simulation/Simulation.java
@@ -19,14 +19,16 @@ public class Simulation {
 	protected String xmlLocation; //location of the XML file representing this simulation
 
 	/*Attributes of an environment	*/
-	protected String simulationName = null, author = null; //the name of this vehicle
-	protected Enviroment enviro; //the environment to use for this simulation
+	protected String simulationName = null, author = null, lastModified = null; //the name of this vehicle
+	protected Enviroment enviro = null; //the environment to use for this simulation
 	protected Vector <Vehicle> vehicles; //the vehicles in this simulation 
 
 	/***** Variables that will affect how the simulation runs *****/
-	protected boolean perishable_vehicles, regenerating_elements, perishable_elements, evolution, sensor_view;
-	protected int repro_method; //reproduction method to be used. 
-	protected int gen_selecion; //genetic selection algorithm to use
+	/* By default all options are off */
+	protected boolean perishable_vehicles = false, regenerating_elements = false,
+	perishable_elements = false, evolution = false, sensor_view = false;
+	protected int repro_method = 0; //reproduction method to be used. 0 - X are valid
+	protected int gen_selection = 0; //genetic selection algorithm to use 0 - X are valid
 
 	public Simulation(){
 		vehicles = new Vector<Vehicle>();
@@ -39,7 +41,7 @@ public class Simulation {
 			String enviro_location = "";
 			vehicles = new Vector<Vehicle>();
 			DOMParser p = new DOMParser();
-			System.out.println("Openiing file: " + xmlLocation);
+			System.out.println("Opening file: " + xmlLocation);
 			p.parse(xmlLocation); //get a parsed version of the file into memory
 			Document dom = p.getDocument();
 
@@ -53,12 +55,13 @@ public class Simulation {
 			NodeList vehiclePaths = dom.getElementsByTagName("VehiclePath");
 			processVehiclePaths(vehiclePaths);
 
-			
+
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 
 	}
+
 	/**
 	 * Take a NodeList representing file paths to vehicles and creates 
 	 * vehicle objects from these paths and adds them to the vehile vector
@@ -98,26 +101,29 @@ public class Simulation {
 			else if(name.equals("Author")){
 				this.setAuthor(node_value);
 			}
+			else if(name.equals("LastModified")){
+				this.setLastModified(node_value);
+			}
 			else if(name.equals("EnvironmentPath")){
 				this.setEnvironment(node_value);
 			}
 			else if(name.equals("perishable_vehicles")){
-				this.setPerishableVehicles((node_value.equals("true")));
+				this.setPerishableVehicles(Boolean.getBoolean(node_value));
 			}
 			else if(name.equals("regenerating_elements")){
-				this.setRegeneratingElements((node_value.equals("true")));
+				this.setRegeneratingElements(Boolean.getBoolean(node_value));
 			}
 			else if(name.equals("perishable_elements")){
-				this.setPerishableElements((node_value.equals("true")));
+				this.setPerishableElements(Boolean.getBoolean(node_value));
 			}
 			else if(name.equals("evolution")){
-				this.setEvolution(node_value.equals("true"));
+				this.setEvolution(Boolean.getBoolean(node_value));
 			}
 			else if(name.equals("reproduction_method")){
 				this.setReproductionMethod(Integer.parseInt(node_value));
 			}
 			else if(name.equals("sensor_view")){
-				this.setSensorView((node_value.equals("true")));
+				this.setSensorView(Boolean.getBoolean(node_value));
 			}
 			else if(name.equals("genetic_selection_method")){
 				this.setGeneticSelectionMethod(Integer.parseInt(node_value));
@@ -137,7 +143,7 @@ public class Simulation {
 	public String getSimulationName() {
 		return simulationName;
 	}
-	
+
 	public String getAuthor(){
 		return this.author;
 	}
@@ -175,7 +181,11 @@ public class Simulation {
 	}
 
 	public int getGeneticSelectionMethod(){
-		return this.gen_selecion;
+		return this.gen_selection;
+	}
+
+	public String getLastModified(){
+		return this.lastModified;
 	}
 
 	/***** Setter Methods *****/
@@ -187,7 +197,7 @@ public class Simulation {
 	public void setSimulationName(String simName) {
 		this.simulationName = simName;
 	}
-	
+
 	public void setAuthor(String name){
 		this.author = name;
 	}
@@ -203,7 +213,7 @@ public class Simulation {
 	public void setEnvironment(Enviroment e){
 		this.enviro = e;
 	}
-	
+
 	public void setEnvironment(String filepath){
 		//TODO Fix environment class so this will work!
 		//this.enviro = new Environment(filepath);
@@ -238,14 +248,27 @@ public class Simulation {
 	public void setGeneticSelectionMethod(int gen){
 		//TODO Find out which genetic selection methods will be
 		//used and make sure the user can only enter a valid one
-		this.gen_selecion = gen;
+		this.gen_selection = gen;
 	}
-	
-	
+
+	public void setLastModified(String timeStamp){
+		this.lastModified = timeStamp;
+	}
+
+	/**
+	 * Method used to find out if the current simulation has enough data to be saved
+	 * @return
+	 */
+	public boolean isSaveable(){
+		return (this.author != null && this.vehicles.size() >= 2 && this.simulationName != null);
+		//TODO Add this back in, when Environment is fixed ->   && this.enviro != null);
+	}
+
 	/**** Other Methods ****/
 	public void printSimDetails(){
 		System.out.println("Name\t" + this.getSimulationName());
 		System.out.println("Author\t" + this.getAuthor());
+		System.out.println("Last Modified\t" + this.getLastModified());
 		System.out.println("Location\t" + this.getXmlLocation());
 		System.out.println("NumVehicles\t" + this.vehicles.size());
 		System.out.println("\tVehicle Paths");
