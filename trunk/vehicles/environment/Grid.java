1d89d7d9813a64ff0d2d7698bd4b0a63db414dba
niall.ohara <niall.ohara@001830dc-046a-11de-89a0-d95f4161952e>
2009-04-25 22:02:34 +0000

    Added grid class to represent the size of the environment

---
+++ trunk/vehicles/environment/Grid.java
@@ -0,0 +1,46 @@
+package vehicles.environment;
+
+/**
+ *
+ * @author Niall O'Hara
+ */
+public class Grid {
+    
+	private int width;
+	private int height;
+    private String desc;
+
+	public Grid(int w, int h, String d) {
+		this.width = w;
+		this.height = h;
+        this.desc = d;
+	}
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public String toString(){
+        return this.width + " " + this.height;
+    }
+}
