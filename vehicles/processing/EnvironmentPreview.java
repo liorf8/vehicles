d90d71510f79f9127404e174037c5c1fc00487a0
karlr42 <karlr42@001830dc-046a-11de-89a0-d95f4161952e>
2009-04-29 18:21:14 +0000

    array out of bound fixed

--- vehicles/processing/EnvironmentPreview.java
+++ vehicles/processing/EnvironmentPreview.java
@@ -11,13 +11,15 @@ import vehicles.environment.EnvironmentElement;
  */
 public class EnvironmentPreview extends PApplet {
 	PImage ground; //background image
-	Environment e;
+	Environment e, spareE;
 
 	public EnvironmentPreview(Environment env){
 		this.e = new Environment(env);
+		this.spareE = new Environment(env);
 	}
 	public void setEnvironment(Environment env){
 		this.e = new Environment(env);
+		this.spareE = new Environment(env);
 	}
 	@Override
 	public void setup() {
@@ -37,42 +39,48 @@ public class EnvironmentPreview extends PApplet {
 	 * @param y panel y size
 	 */
 	public void updateSize(int x, int y){
+
 		float scaleFactor = (float)this.width / (float) x ;
 		for(int i = 0;i<e.getElements().size();i++){
 			e.getElements().elementAt(i).
-				setRadius((int)(e.getElements().elementAt(i).getRadius() / scaleFactor));
+			setRadius((int)(e.getElements().elementAt(i).getRadius() / scaleFactor));
 			e.getElements().elementAt(i).
-				setXpos(e.getElements().elementAt(i).getXpos() / scaleFactor);
+			setXpos(e.getElements().elementAt(i).getXpos() / scaleFactor);
 			e.getElements().elementAt(i).
-				setYpos(e.getElements().elementAt(i).getYpos() / scaleFactor);
+			setYpos(e.getElements().elementAt(i).getYpos() / scaleFactor);
 			e.getElements().elementAt(i).
-				setStrength((int)(e.getElements().elementAt(i).getStrength() / scaleFactor));			
+			setStrength((int)(e.getElements().elementAt(i).getStrength() / scaleFactor));                  
 		}
 		size(x,y);
+		this.e = new Environment(spareE);
 	}
 
-	
+
 	public void updateGround() {
-			float sum;
-			int c, r, g, b;
-			int px = 1;
+		println("update Ground called");
+		float sum;
+		int c, r, g, b;
+		int px = 1;
 
-			ground = new PImage(width, height);
-			for (int i = 0; i < width; i += px) {
-				for (int k = 0; k < height; k += px) { //process every pixel in the image
+		ground = new PImage(width, height);
+		for (int i = 0; i < width; i += px) {
+			for (int k = 0; k < height; k += px) { //process every pixel in the image
 
-					sum = 0;
+				sum = 0;
 				r = 0;
 				g = 0;
 				b = 0;
 				for (int m = 0; m < e.getElements().size(); m++) {
-					ProcessingEnviroElement temp = 
+					ProcessingEnviroElement temp  = null;
+					try{
+						temp =
 							new ProcessingEnviroElement(this,e.getElements().elementAt(m),0);
-					//pass this pixel's position
-					sum += temp.getIntensityAtPoint(i, k);
-					r += temp.getRedAtPoint(i, k);
-					g += temp.getGreenAtPoint(i, k);
-					b += temp.getBlueAtPoint(i, k);
+						//pass this pixel's position
+						sum += temp.getIntensityAtPoint(i, k);
+						r += temp.getRedAtPoint(i, k);
+						g += temp.getGreenAtPoint(i, k);
+						b += temp.getBlueAtPoint(i, k);
+					}catch(Exception e){print("null\n");}
 					//sum up intensity of elementVector until it reachrs one
 					if (sum >= 1) {
 						break;
@@ -90,10 +98,7 @@ public class EnvironmentPreview extends PApplet {
 								color(r, g, b / 8, c)); //r,g,b
 					}
 				}
-
-				}
 			}
-			
 		}
-
 	}
+}
