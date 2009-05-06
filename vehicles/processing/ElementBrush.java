54448c4d60738ceb4674f257fcae5000902ca21d
niall.ohara <niall.ohara@001830dc-046a-11de-89a0-d95f4161952e>
2009-05-06 16:00:11 +0000

    Some tidy up code

--- vehicles/processing/ElementBrush.java
+++ vehicles/processing/ElementBrush.java
@@ -1,37 +1,38 @@
 package vehicles.processing;
+
 import vehicles.environment.*;
+
 /**
  *
  * @author Niall O'Hara
  */
- public class ElementBrush {
-	 EnvironmentElement currentlySelected;
+public class ElementBrush {
 
-     public ElementBrush(){
-	 }
+    EnvironmentElement currentlySelected;
 
-	 public ElementBrush(EnvironmentElement p_ele){
-		 this.currentlySelected = p_ele;
-	 }
-	 
-	 /**
-	  * Get the current "paint" on the brush
-	  * @return the enviornment element currently in use
-	  */
-	public EnvironmentElement getCurrentlySelected() {
-		return currentlySelected;
-	}
-	/**
-	 * Apply "paint" to the brush
-	 * @param currentlySelected the environment element to set
-	 */
-	public void setCurrentlySelected(EnvironmentElement currentlySelected) {
-		//System.out.println("setCurrentlySelectes() : change to "+this.getCurrentlySelected());
+    public ElementBrush() {
+    }
 
-		this.currentlySelected = new EnvironmentElement(currentlySelected);
-	}
-	 
-	 
-     
- }
+    public ElementBrush(EnvironmentElement p_ele) {
+        this.currentlySelected = p_ele;
+    }
+
+    /**
+     * Get the current "paint" on the brush
+     * @return the enviornment element currently in use
+     */
+    public EnvironmentElement getCurrentlySelected() {
+        return currentlySelected;
+    }
+
+    /**
+     * Apply "paint" to the brush
+     * @param currentlySelected the environment element to set
+     */
+    public void setCurrentlySelected(EnvironmentElement currentlySelected) {
+        //System.out.println("setCurrentlySelectes() : change to "+this.getCurrentlySelected());
+
+        this.currentlySelected = new EnvironmentElement(currentlySelected);
+    }
+}
 
--- vehicles/processing/ElementBrushPreview.java
+++ vehicles/processing/ElementBrushPreview.java
@@ -6,32 +6,35 @@ import processing.core.PApplet;
 import vehicles.environment.EnvironmentElement;
 
 @SuppressWarnings("serial")
-public class ElementBrushPreview extends PApplet{
-	ProcessingEnviroElement pev;	
+public class ElementBrushPreview extends PApplet {
 
-	public ProcessingEnviroElement getPev() {
-		return pev;
-	}
+    ProcessingEnviroElement pev;
 
-	public void setPev(EnvironmentElement pev) {
-		this.pev = new ProcessingEnviroElement(this,pev,0);
-		this.draw();
-	}
+    public ProcessingEnviroElement getPev() {
+        return pev;
+    }
 
-	public void setup(){
-		try{
-			size(pev.getRadius() + 10,pev.getRadius() + 10);
-		}catch(NullPointerException e){}
-		background(Color.BLACK.getRGB());
-		smooth();
-	}
+    public void setPev(EnvironmentElement pev) {
+        this.pev = new ProcessingEnviroElement(this, pev, 0);
+        this.draw();
+    }
 
-	public void draw(){
-		try{
-			size(pev.getRadius() + 10,pev.getRadius() + 10);
-			pev.previewDraw(width /2, height /2);
-		}catch(NullPointerException e){}
-		noLoop();
+    public void setup() {
+        try {
+            size(pev.getRadius() + 10, pev.getRadius() + 10);
+        } catch (NullPointerException e) {
+        }
+        background(Color.BLACK.getRGB());
+        smooth();
+    }
 
-	}
+    public void draw() {
+        try {
+            size(pev.getRadius() + 10, pev.getRadius() + 10);
+            pev.previewDraw(width / 2, height / 2);
+        } catch (NullPointerException e) {
+        }
+        noLoop();
+
+    }
 }
